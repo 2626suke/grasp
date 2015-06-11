96c92b1fd4b6fa1f9a0c80f7822b91d22a17fe07
2626suke <2626suke@gmail.com>
2015-06-11 22:04:51 +0900

    本番用設定

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -1,14 +1,14 @@
 <?php
+
 /**
  * The production database settings. These get merged with the global settings.
  */
-
 return array(
-	'default' => array(
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
-		),
-	),
+    'default' => array(
+        'connection' => array(
+            'dsn' => 'mysql:host=mysql025.phy.lolipop.lan;dbname=LAA0501775-grasp',
+            'username' => 'LAA0501775',
+            'password' => $_SERVER['MYSQL_PASSWORD'],
+        ),
+    ),
 );
