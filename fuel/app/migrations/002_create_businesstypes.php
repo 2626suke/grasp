eda53fc4e517702305cf0ac25da842a482ed24f4
2626suke <2626suke@gmail.com>
2015-06-14 22:33:41 +0900

    マイグレーション見直し

--- fuel/app/migrations/002_create_businesstypes.php
+++ fuel/app/migrations/002_create_businesstypes.php
@@ -8,10 +8,21 @@ class Create_businesstypes
     public function up()
     {
         \DBUtil::create_table('businesstypes', array(
-            'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
-            'name' => array('constraint' => 10, 'type' => 'varchar'),
-            'kbn' => array('constraint' => 2, 'type' => 'varchar')
-                ), array('id'), false, 'InnoDB', 'utf8_general_ci');
+            'id' => array(
+                'constraint' => 11,
+                'type' => 'int',
+                'auto_increment' => true,
+                'unsigned' => true
+            ),
+            'name' => array(
+                'constraint' => 10,
+                'type' => 'varchar'
+            ),
+            'kbn' => array(
+                'constraint' => 2,
+                'type' => 'varchar'
+            )
+                ), array('id'));
     }
 
     public function down()
--- fuel/app/migrations/003_create_clients.php
+++ fuel/app/migrations/003_create_clients.php
@@ -8,37 +8,88 @@ class Create_clients
     public function up()
     {
         \DBUtil::create_table('clients', array(
-            'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
-            'name' => array('constraint' => 50, 'type' => 'varchar'),
-            'business_form_id' => array('constraint' => 11, 'type' => 'int', 'unsigned' => true),
-            'business_type_id' => array('constraint' => 11, 'type' => 'int', 'unsigned' => true),
-            'zip' => array('constraint' => 7, 'type' => 'char', 'null' => true),
-            'address' => array('constraint' => 100, 'type' => 'varchar', 'null' => true),
-            'tel' => array('constraint' => 15, 'type' => 'varchar', 'null' => true),
-            'fax' => array('constraint' => 15, 'type' => 'varchar', 'null' => true),
-            'mail' => array('constraint' => 100, 'type' => 'varchar', 'null' => true),
-            'url' => array('constraint' => 100, 'type' => 'varchar', 'null' => true),
-            'employee_num' => array('constraint' => 7, 'type' => 'varchar', 'null' => true),
-            'establish_date' => array('type' => 'date', 'null' => true),
-            'capital' => array('constraint' => 11, 'type' => 'varchar', 'null' => true),
-            'account_date' => array('type' => 'date', 'null' => true),
-            'created_at' => array('type' => 'timestamp', 'null' => true),
-            'updated_at' => array('type' => 'timestamp', 'null' => true),
-            'deleted_at' => array('type' => 'timestamp', 'null' => true)
-                ), array('id'), false, 'InnoDB', 'utf8_general_ci', array(array(
-                'key' => 'business_form_id',
-                'reference' => array(
-                    'table' => 'businessforms',
-                    'column' => 'id'
-                )
-            ), array(
-                'key' => 'business_type_id',
-                'reference' => array(
-                    'table' => 'businesstypes',
-                    'column' => 'id'
-                )
+            'id' => array(
+                'constraint' => 11,
+                'type' => 'int',
+                'auto_increment' => true,
+                'unsigned' => true
+            ),
+            'name' => array(
+                'constraint' => 50,
+                'type' => 'varchar'
+            ),
+            'business_form_id' => array(
+                'constraint' => 11,
+                'type' => 'int'
+            ),
+            'business_type_id' => array(
+                'constraint' => 11,
+                'type' => 'int'
+            ),
+            'zip' => array(
+                'constraint' => 7,
+                'type' => 'char',
+                'null' => true
+            ),
+            'address' => array(
+                'constraint' => 100,
+                'type' => 'varchar',
+                'null' => true
+            ),
+            'tel' => array(
+                'constraint' => 15,
+                'type' => 'varchar',
+                'null' => true
+            ),
+            'fax' => array(
+                'constraint' => 15,
+                'type' => 'varchar',
+                'null' => true
+            ),
+            'mail' => array(
+                'constraint' => 100,
+                'type' => 'varchar',
+                'null' => true
+            ),
+            'url' => array(
+                'constraint' => 100,
+                'type' => 'varchar',
+                'null' => true
+            ),
+            'employee_num' => array(
+                'constraint' => 7,
+                'type' => 'varchar',
+                'null' => true
+            ),
+            'establish_date' => array(
+                'type' => 'date',
+                'null' => true
+            ),
+            'capital' => array(
+                'constraint' => 11,
+                'type' => 'varchar',
+                'null' => true
+            ),
+            'account_date' => array(
+                'type' => 'date',
+                'null' => true
+            ),
+            'created_at' => array(
+                'constraint' => 11,
+                'type' => 'int',
+                'null' => true
+            ),
+            'updated_at' => array(
+                'constraint' => 11,
+                'type' => 'int',
+                'null' => true
+            ),
+            'deleted_at' => array(
+                'constraint' => 11,
+                'type' => 'int',
+                'null' => true
             )
-        ));
+                ), array('id'));
     }
 
     public function down()
